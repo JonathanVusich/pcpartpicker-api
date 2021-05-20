cd0d9fd96bd36484bef6e9d9a14ae1112519395e
Jonathan Vusich <apianist16@gmail.com>
2021-05-20 09:48:10 -0500

    Setup API

--- pcpartpicker-api/routing.py
+++ pcpartpicker-api/routing.py
@@ -1,11 +1,15 @@
 from sanic import Sanic
 from sanic.response import json
 from sanic.response import HTTPResponse
+from litedb import DiskDatabase
+
+from pcpartpicker.mappings import part_classes
 
 app = Sanic.get_app()
 
 
 @app.route('/<region:string>/<part:string>')
 async def tag_handler(region: str, part: str) -> HTTPResponse:
-    part_data = await get_database_request(region, part)
+    database: DiskDatabase = app.ctx.db[region]
+    part_data = [item for item in database.select(part_classes[part])]
     return json(part_data)
--- pcpartpicker-api/server.py
+++ pcpartpicker-api/server.py
@@ -1,4 +1,13 @@
 from sanic import Sanic
+from pcpartpicker import API
+from litedb import DiskDatabase
 
 app = Sanic()
+api = API()
+
+app.ctx.db = {}
+
+for region in api.supported_regions:
+    app.ctx.db[region] = DiskDatabase(f"/var/cache/pcpartpicker-api/{region}")
+
 app.run()
