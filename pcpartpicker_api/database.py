bde45fcdd69d8361a083e61479f383173f91e33d
Jonathan Vusich <apianist16@gmail.com>
2021-07-14 15:40:45 -0500

    Typing fix

--- pcpartpicker_api/database.py
+++ pcpartpicker_api/database.py
@@ -1,12 +1,14 @@
 import sqlite3
 
+from typing import List
+
 
 class Database:
 
     def __init__(self, database: str):
         self._connection = sqlite3.connect(database)
 
-    def retrieve_items(self, region: str, part_type: str) -> list[str]:
+    def retrieve_items(self, region: str, part_type: str) -> List[str]:
         cursor = self._connection.cursor()
         cursor.execute(f'select * from {region}_{part_type}')
         return cursor.fetchall()
