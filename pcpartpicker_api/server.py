1634a792e8107f72f2b6385b7da0c10687d44d25
Jonathan Vusich <apianist16@gmail.com>
2021-07-14 15:42:17 -0500

    Fix module

--- pcpartpicker_api/server.py
+++ pcpartpicker_api/server.py
@@ -4,17 +4,18 @@ from sanic import Sanic, HTTPResponse, json
 
 from .database import Database
 
-if __name__ == "__main__":
-    parser = ArgumentParser(description="Sanic API for fetching cached pcpartpicker data.")
-    parser.add_argument("--database", type=str)
-    args = parser.parse_args()
+parser = ArgumentParser(description="Sanic API for fetching cached pcpartpicker data.")
+parser.add_argument("--database", type=str)
+args = parser.parse_args()
 
-    app = Sanic(name="pcpartpicker-api")
-    database = Database(args.database)
+app = Sanic(name="pcpartpicker-api")
+database = Database(args.database)
 
-    @app.get('/<region:string>/<part:string>')
-    async def tag_handler(region: str, part: str) -> HTTPResponse:
-        parts = database.retrieve_items(region, part)
-        return json(parts)
 
-    app.run()
+@app.get('/<region:string>/<part:string>')
+async def tag_handler(region: str, part: str) -> HTTPResponse:
+    parts = database.retrieve_items(region, part)
+    return json(parts)
+
+
+app.run()
