c6dd9342dba733e34d869983e6e04c81adeda083
Jonathan Vusich <apianist16@gmail.com>
2021-07-14 15:16:41 -0500

    WIP

--- pcpartpicker_api/server.py
+++ pcpartpicker_api/server.py
@@ -1,9 +1,20 @@
-from sanic import Sanic
+from argparse import ArgumentParser
+
+from sanic import Sanic, HTTPResponse, json
 
 from database import Database
 
-app = Sanic()
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Sanic API for fetching cached pcpartpicker data.")
+    parser.add_argument("--database", type=str)
+    args = parser.parse_args()
 
-app.ctx.db = {"database", Database("/var/cache/pcpartpicker-api.db")}
+    app = Sanic(name="pcpartpicker-api")
+    database = Database(args.database)
 
-app.run()
+    @app.get('/<region:string>/<part:string>')
+    async def tag_handler(region: str, part: str) -> HTTPResponse:
+        parts = database.retrieve_items(region, part)
+        return json(parts)
+
+    app.run()
