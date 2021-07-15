3f18b0ac5d9010ff22da8a689d16784455318844
Jonathan Vusich <apianist16@gmail.com>
2021-07-15 07:42:43 -0500

    Add table sanitization

--- pcpartpicker_api/database.py
+++ pcpartpicker_api/database.py
@@ -2,13 +2,18 @@ import sqlite3
 
 from typing import List
 
+from pcpartpicker import API
+
 
 class Database:
 
     def __init__(self, database: str):
         self._connection = sqlite3.connect(database)
+        self._api = API()
 
     def retrieve_items(self, region: str, part_type: str) -> List[str]:
-        cursor = self._connection.cursor()
-        cursor.execute(f'select * from {region}_{part_type}')
-        return cursor.fetchall()
+        if region in self._api.supported_regions and part_type in self._api.supported_parts:
+            cursor = self._connection.cursor()
+            cursor.execute(f'select * from {region}_{part_type}')
+            return cursor.fetchall()
+        return []
